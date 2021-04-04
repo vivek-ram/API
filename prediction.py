5412a4392882236e139b65d268bddfdd70d78bd7
vivek <viveksivaram00@gmail.com>
2021-04-04 04:25:07 +0100

    Updated with probability

--- prediction.py
+++ prediction.py
@@ -9,7 +9,7 @@ import io
 input_shape = (224,224)
 
 def load_model():
-    model = tf.keras.models.load_model("model_V1.h5")
+    model = tf.keras.models.load_model("vgg16_B1.h5")
     return model
 
 model = load_model()
@@ -25,13 +25,22 @@ def preprocess(image : Image.Image):
 
     image = image.resize(input_shape)
     image = np.asfarray(image)
-    image = image / 127.5 - 1.0
+    image = image / 225
     image = np.expand_dims(image,0)
     return image
 
 
 def predict(image: np.ndarray):
-    label = {'akiec': 0, 'bcc': 1, 'bkl': 2, 'df': 3, 'mel': 4, 'melanoma': 5, 'nevus': 6, 'nv': 7, 'seborrheic_keratosis': 8, 'vasc': 9}
+    label = {'Bowen’s disease': 0, 'Basal cell carinoma': 1, 'benign keratosis': 2, 'dermatofibroma': 3, 'melanoma': 4, 'melanocytic nevi': 5, 'vascular lesions': 6}
     classes = list(label)
     predictions = classes[np.argmax(model.predict(image))]
-    return predictions
+    prob = model.predict(image)
+    temp = ['Bowen’s disease','Basal cell carinoma','benign keratosis','dermatofibroma','melanoma','melanocytic nevi','vascular lesions']
+    dum = 0
+    for i in temp:
+        label[i] = str(round(prob[0][dum]*100,2)) + '%'
+        dum += 1
+    dic = {'prediction':predictions,'probability':label}
+    return dic
+
+
